850d252e8dc94a63c20878be5eb73699b2a50164
Jeff Sherin <jeffrey.sherin@gmail.com>
2023-04-27 11:26:50 -0400

    Initial Commit

---
+++ MatchedGeometry/MatchedGeometryApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatchedGeometryApp.swift
+//  MatchedGeometry
+//
+//  Created by Jeffrey Sherin on 4/27/23.
+//
+
+import SwiftUI
+
+@main
+struct MatchedGeometryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
